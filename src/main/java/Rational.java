658c15f69aa619b1de1a644b13785e89b5a7b961
jakezhang <24jzhang@athenian.org>
2021-10-20 11:15:11 -0700

    done

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -26,33 +26,82 @@ public class Rational {
     // (When you write the simplify method later on, you should
     // also call it in this method to return the simplified result)
     public static Rational add(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int a=r.numerator;
+        int b=r.denominator;
+        int c=s.numerator;
+        int d=s.denominator;
+        int newNum=a*d+c*b;
+        int newDon=b*d;
+        for (int i=100;i>1;i--){
+            if (newNum%i==0 && newDon%i==0){
+                newNum=newNum/i;
+                newDon=newDon/i;
+            }
+        }
+        return new Rational(newNum,newDon);
     }
 
     // This method takes two Rationals, subtracts thems up, 
     // and returns a Rational equal to the difference
     public static Rational subtract(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int a=r.numerator;
+        int b=r.denominator;
+        int c=s.numerator;
+        int d=s.denominator;
+        int newNum=a*d-c*b;
+        int newDon=b*d;
+        for (int i=100;i>1;i--){
+            if (newNum%i==0 && newDon%i==0){
+                newNum=newNum/i;
+                newDon=newDon/i;
+            }
+        }
+        return new Rational(newNum,newDon);
     }
     
     public static Rational multiply(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int a=r.numerator;
+        int b=r.denominator;
+        int c=s.numerator;
+        int d=s.denominator;
+        int newNum=a*c;
+        int newDon=b*d;
+        for (int i=100;i>1;i--){
+            if (newNum%i==0 && newDon%i==0){
+                newNum=newNum/i;
+                newDon=newDon/i;
+            }
+        }
+        return new Rational(newNum,newDon);
     }
     
     public static Rational divide(Rational r, Rational s) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int a=r.numerator;
+        int b=r.denominator;
+        int c=s.numerator;
+        int d=s.denominator;
+        int newNum=a*d;
+        int newDon=b*c;
+        for (int i=100;i>1;i--){
+            if (newNum%i==0 && newDon%i==0){
+                newNum=newNum/i;
+                newDon=newDon/i;
+            }
+        }
+        return new Rational(newNum,newDon);
     }
 
     // Finds the greatest common factor between a and b
     // To find the greatest common factor, find the largest number x
     // such that a and b are both multiples of x
     public static int greatestCommonFactor(int a, int b){
-        // REPLACE WITH YOUR CODE HERE
-        return 1;
+        int temp=1;
+        for (int i=1;i<=Math.min(a,b);i++){
+            if (a%i==0 && b%i==0){
+                temp=i;
+            }
+        }
+        return temp;
     }
 
     // This method is given a rational, and returns a simplified version
@@ -61,8 +110,9 @@ public class Rational {
     // e.g. simplify(2/4) => 1/2
     //      simplify(1/2) => 1/2
     public static Rational simplify(Rational r) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int a=r.numerator;
+        int b=r.denominator;
+        return new Rational(a/greatestCommonFactor(a,b),b/greatestCommonFactor(a,b));
     }
 
     // This following method is NOT static, we'll talk about it next class!
@@ -70,6 +120,15 @@ public class Rational {
     @Override
     public String toString() {
         return this.numerator + "/" + this.denominator;
+    }
+
+    public static void main(String[] args){
+        System.out.println(greatestCommonFactor(7,28));
+        System.out.println(greatestCommonFactor(64,24));
+        System.out.println(greatestCommonFactor(6,8));
+        System.out.println(simplify(new Rational(2, 6)));
+        System.out.println(simplify(new Rational(12, 628)));
+
     }
 }
 
